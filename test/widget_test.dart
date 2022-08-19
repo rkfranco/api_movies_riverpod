9d9c6aaadd8e5e803ce64484c5e47e6c0118351b
Rodrigo Kapulka Franco <Rodrigo.Franco@proway.treina>
2022-08-19 10:05:55 -0300

    [feat] tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,17 +14,8 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await Future.delayed(const Duration(seconds: 2));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byType(Container));
   });
 }
